c3482f53e618a3427b394845ba206049fc7455f1
atbee <atb@odds.team>
2020-10-08 22:47:33 +0700

    Add subtract function :bee:

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 
-from .calc import add
+from .calc import add, subtract
 
 
 class CalcTest(TestCase):
 
-    def test_add_number(self):
+    def test_add_numbers(self):
         self.assertEqual(add(3, 4), 7)
+
+    def test_subtract_numbers(self):
+        self.assertEqual(subtract(8, 2), 6)
